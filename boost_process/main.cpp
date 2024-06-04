71d856f1b0bca975072f8ed14bd10c735428936b
Creeper-xie <Creeperxie@outlook.com>
2024-06-05 03:47:18 +0800

    [Init]

---
+++ boost_process/main.cpp
@@ -0,0 +1,35 @@
+#include <boost/process.hpp>
+#include <boost/process/detail/child_decl.hpp>
+#include <boost/process/io.hpp>
+#include <boost/process/pipe.hpp>
+#include <iomanip>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+namespace process=boost::process;
+
+int main(const int argc,const char* argv[]){
+//    std::vector<std::string> command;
+    std::string command;
+    std::vector<std::string> args(argv + 1, argv + argc);
+    for (std::string a : args){
+        command += ' ' + a;
+//        std::cout << command <<std::endl;
+//        << sizeof(argv[i]) << std::endl;
+//        command.push_back(argv[i]);
+    }
+//    std::cout << command << std::endl;
+    boost::process::ipstream pipe_stream;
+    boost::process::child cmd(command, boost::process::std_out > pipe_stream);
+
+    std::string line;
+
+    while (pipe_stream && std::getline(pipe_stream, line) && !line.empty())
+    std::cerr << line << std::endl;
+
+    cmd.wait();
+
+}
