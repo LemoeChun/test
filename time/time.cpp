756a0b3758fddc3eed8ee387a2cb39b6865b24fe
Creeper-xie <Creeperxie@outlook.com>
2024-07-17 04:07:42 +0000

    修改：     time.cpp

--- time/time.cpp
+++ time/time.cpp
@@ -3,11 +3,10 @@
 #include <boost/filesystem.hpp>
 #include <iostream>
 #include <ostream>
-// #include <leveldb/>
 
-int main(){
+int main(const int agrc, const char** argv){
     
-    auto path = boost::filesystem::path("time.cpp");
+    auto path = boost::filesystem::path(argv[1]);
     std::time_t time = boost::filesystem::last_write_time(path);
     std::cout << time << std::endl;
 }
