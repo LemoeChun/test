c2920fde76f4506efe4bf288e7f6ba353d951e98
Creeper-xie <Creeperxie@outlook.com>
2025-01-27 17:46:37 +0000

    toml++: test

--- toml++/main.cpp
+++ toml++/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iterator>
+#include <optional>
 #include <ostream>
 #include <toml++/toml.hpp>
 
@@ -8,6 +9,8 @@ auto main(int argc,const char** argv) -> int
 try{
     toml::table config = toml::parse_file(argv[1]);
     std::cout<<config<<std::endl;
+    std::optional<std::string> token = config["bot"]["token"].value<std::string>();
+    std::cout << "token:" << *token<< std::endl;
 } catch (const toml::parse_error& err)
     {
         std::cerr<< err << std::endl;
